3befe94abbdcae4cfd6a5c62bccf23c92cbc98ab
Amaury Carrade <amaury@carrade.eu>
2014-12-12 18:15:30 +0100

    Durability

--- src/main/java/eu/carrade/amaury/BelovedBlocks/BBListener.java
+++ src/main/java/eu/carrade/amaury/BelovedBlocks/BBListener.java
@@ -1,10 +1,13 @@
 package eu.carrade.amaury.BelovedBlocks;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.inventory.ItemStack;
 
 public class BBListener implements Listener {
 	
@@ -18,12 +21,22 @@ public class BBListener implements Listener {
 	public void onPlayerInteract(PlayerInteractEvent ev) {
 		if(!ev.hasBlock() || !ev.hasItem()) return;
 		
-		if(ev.getItem().getType() == Material.DIAMOND_HOE
-				&& ev.getItem().getItemMeta().getDisplayName().equals(p.getConfig().getString("tool.name"))) {
+		ItemStack item = ev.getItem();
+		Block block = ev.getClickedBlock();
+		Player player = ev.getPlayer();
+		
+		if(item.getType() == Material.DIAMOND_HOE
+				&& item.getItemMeta().getDisplayName().equals(p.getConfig().getString("tool.name"))) {
 			switch(ev.getClickedBlock().getType()) {
 				case DOUBLE_STEP:
 				//case DOUBLE_STONE_SLAB2:
-				ev.getClickedBlock().setData((byte) (ev.getClickedBlock().getData() + 8));
+				block.setData((byte) (ev.getClickedBlock().getData() + 8));
+				
+				if(player.getGameMode() != GameMode.CREATIVE) {
+					item.setDurability((short) (item.getDurability() + 1));
+					player.getInventory().setItemInHand(item);
+					player.updateInventory();
+				}
 				break;
 			default:
 				break;
