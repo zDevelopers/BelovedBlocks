24f373d8614a4fa418843ac533a307379e309446
Amaury Carrade <amaury@carrade.eu>
2014-12-20 15:59:48 +0100

    Rewrote the way "squared" recipes are added.
    Removed copy/paste.

--- src/main/java/eu/carrade/amaury/BelovedBlocks/BBRecipes.java
+++ src/main/java/eu/carrade/amaury/BelovedBlocks/BBRecipes.java
@@ -1,6 +1,7 @@
 package eu.carrade.amaury.BelovedBlocks;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
 public class BBRecipes {
@@ -10,16 +11,51 @@ public class BBRecipes {
 	public BBRecipes(BelovedBlocks plugin){
 		p = plugin;
 		
+		/** Tools **/
+		
 		registerTool();
-		registerStone();
-		registerSand();
-		registerRedsand();
-		registerOak();
-		registerSpruce();
-		registerBirch();
-		registerJungle();
-		registerAcacia();
-		registerDarkOak();
+		
+		
+		/** Slabs **/
+		
+		if(p.getConfig().getBoolean("blocks.slabs.stone.craftable")) {
+			registerSquaredRecipe(Material.STEP, 0, p.getSmoothStoneItem(2));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.slabs.sandstone.craftable")) {
+			registerSquaredRecipe(Material.STEP, 1, p.getSmoothSandstoneItem(2));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.slabs.red_sandstone.craftable")) {
+			registerSquaredRecipe(Material.STONE_SLAB2, 0, p.getSmoothRedSandstoneItem(2));
+		}
+		
+		
+		/** Logs **/
+		
+		if(p.getConfig().getBoolean("blocks.logs.oak.craftable")) {
+			registerSquaredRecipe(Material.LOG, 0, p.getSmoothOakItem(4));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.logs.spruce.craftable")) {
+			registerSquaredRecipe(Material.LOG, 1, p.getSmoothSpruceItem(4));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.logs.birch.craftable")) {
+			registerSquaredRecipe(Material.LOG, 2, p.getSmoothBirchItem(4));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.logs.jungle.craftable")) {
+			registerSquaredRecipe(Material.LOG, 3, p.getSmoothJungleItem(4));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.logs.acacia.craftable")) {
+			registerSquaredRecipe(Material.LOG_2, 0, p.getSmoothAcaciaItem(4));
+		}
+		
+		if(p.getConfig().getBoolean("blocks.logs.dark_oak.craftable")) {
+			registerSquaredRecipe(Material.LOG_2, 1, p.getSmoothDarkOakItem(4));
+		}
 	}
 	
 	
@@ -58,183 +94,27 @@ public class BBRecipes {
 	}
 	
 	/**
-	 * Registers the recipes for the smooth stone double-slab item.
+	 * Registers a recipe to craft {@code result} from the given ingredient
+	 * placed in a 4*4 grid.
+	 * 
+	 * @param ingredientMaterial The ingredient (material).
+	 * @param materialDataValue The ingredient (data value).
+	 * @param result The result.
 	 */
-	public void registerStone() {
-		if(p.getConfig().getBoolean("blocks.slabs.stone.craftable")) {
-			ShapedRecipe smoothStoneSlabRecipe = new ShapedRecipe(p.getSmoothStoneItem(2));
-			
-			smoothStoneSlabRecipe.shape("SS ", "SS ", "   ");
-			smoothStoneSlabRecipe.setIngredient('S', Material.STEP, 0);
-			p.getServer().addRecipe(smoothStoneSlabRecipe);
-			
-			smoothStoneSlabRecipe.shape(" SS", " SS", "   ");
-			p.getServer().addRecipe(smoothStoneSlabRecipe);
-			
-			smoothStoneSlabRecipe.shape("   ", " SS", " SS");
-			p.getServer().addRecipe(smoothStoneSlabRecipe);
-			
-			smoothStoneSlabRecipe.shape("   ", "SS ", "SS ");
-			p.getServer().addRecipe(smoothStoneSlabRecipe);
-		}
+	public void registerSquaredRecipe(Material ingredientMaterial, int ingredientDataValue, ItemStack result) {
+		ShapedRecipe recipe = new ShapedRecipe(result);
+		
+		recipe.shape("II ", "II ", "   ");
+		recipe.setIngredient('I', ingredientMaterial, ingredientDataValue);
+		p.getServer().addRecipe(recipe);
+		
+		recipe.shape(" II", " II", "   ");
+		p.getServer().addRecipe(recipe);
+		
+		recipe.shape("   ", " II", " II");
+		p.getServer().addRecipe(recipe);
+		
+		recipe.shape("   ", "II ", "II ");
+		p.getServer().addRecipe(recipe);
 	}
-	
-	/**
-	 * Registers the recipes for the smooth sandstone double-slab item.
-	 */
-	public void registerSand() {
-		if(p.getConfig().getBoolean("blocks.slabs.sandstone.craftable")) {
-			ShapedRecipe smoothSandstoneSlabRecipe = new ShapedRecipe(p.getSmoothSandstoneItem(2));
-			
-			smoothSandstoneSlabRecipe.shape("SS ", "SS ", "   ");
-			smoothSandstoneSlabRecipe.setIngredient('S', Material.STEP, 1);
-			p.getServer().addRecipe(smoothSandstoneSlabRecipe);
-			
-			smoothSandstoneSlabRecipe.shape(" SS", " SS", "   ");
-			p.getServer().addRecipe(smoothSandstoneSlabRecipe);
-			
-			smoothSandstoneSlabRecipe.shape("   ", " SS", " SS");
-			p.getServer().addRecipe(smoothSandstoneSlabRecipe);
-			
-			smoothSandstoneSlabRecipe.shape("   ", "SS ", "SS ");
-			p.getServer().addRecipe(smoothSandstoneSlabRecipe);
-		}
-	}
-	
-	/**
-	 * Registers the recipes for the smooth red sandstone double-slab item.
-	 */
-	public void registerRedsand() {
-		if(p.getConfig().getBoolean("blocks.slabs.red_sandstone.craftable")) {
-			ShapedRecipe smoothRedSandstoneSlabRecipe = new ShapedRecipe(p.getSmoothRedSandstoneItem(2));
-	
-			smoothRedSandstoneSlabRecipe.shape("SS ", "SS ", "   ");
-			smoothRedSandstoneSlabRecipe.setIngredient('S', Material.STONE_SLAB2, 0);
-			p.getServer().addRecipe(smoothRedSandstoneSlabRecipe);
-			
-			smoothRedSandstoneSlabRecipe.shape(" SS", " SS", "   ");
-			p.getServer().addRecipe(smoothRedSandstoneSlabRecipe);
-			
-			smoothRedSandstoneSlabRecipe.shape("   ", " SS", " SS");
-			p.getServer().addRecipe(smoothRedSandstoneSlabRecipe);
-			
-			smoothRedSandstoneSlabRecipe.shape("   ", "SS ", "SS ");
-			p.getServer().addRecipe(smoothRedSandstoneSlabRecipe);
-		}
-	}
-	
-	public void registerOak() {
-		if(p.getConfig().getBoolean("blocks.logs.oak.craftable")) {
-			ShapedRecipe smoothOakLogRecipe = new ShapedRecipe(p.getSmoothOakItem(4));
-	
-			smoothOakLogRecipe.shape("LL ", "LL ", "   ");
-			smoothOakLogRecipe.setIngredient('L', Material.LOG, 0);
-			p.getServer().addRecipe(smoothOakLogRecipe);
-			
-			smoothOakLogRecipe.shape(" LL", " LL", "   ");
-			p.getServer().addRecipe(smoothOakLogRecipe);
-			
-			smoothOakLogRecipe.shape("   ", " LL", " LL");
-			p.getServer().addRecipe(smoothOakLogRecipe);
-			
-			smoothOakLogRecipe.shape("   ", "LL ", "LL ");
-			p.getServer().addRecipe(smoothOakLogRecipe);
-		}
-	}
-	
-	public void registerSpruce() {
-		if(p.getConfig().getBoolean("blocks.logs.spruce.craftable")) {
-			ShapedRecipe smoothSpruceLogRecipe = new ShapedRecipe(p.getSmoothSpruceItem(4));
-	
-			smoothSpruceLogRecipe.shape("LL ", "LL ", "   ");
-			smoothSpruceLogRecipe.setIngredient('L', Material.LOG, 1);
-			p.getServer().addRecipe(smoothSpruceLogRecipe);
-			
-			smoothSpruceLogRecipe.shape(" LL", " LL", "   ");
-			p.getServer().addRecipe(smoothSpruceLogRecipe);
-			
-			smoothSpruceLogRecipe.shape("   ", " LL", " LL");
-			p.getServer().addRecipe(smoothSpruceLogRecipe);
-			
-			smoothSpruceLogRecipe.shape("   ", "LL ", "LL ");
-			p.getServer().addRecipe(smoothSpruceLogRecipe);
-		}
-	}
-	
-	public void registerBirch() {
-		if(p.getConfig().getBoolean("blocks.logs.birch.craftable")) {
-			ShapedRecipe smoothBirchLogRecipe = new ShapedRecipe(p.getSmoothBirchItem(4));
-	
-			smoothBirchLogRecipe.shape("LL ", "LL ", "   ");
-			smoothBirchLogRecipe.setIngredient('L', Material.LOG, 2);
-			p.getServer().addRecipe(smoothBirchLogRecipe);
-			
-			smoothBirchLogRecipe.shape(" LL", " LL", "   ");
-			p.getServer().addRecipe(smoothBirchLogRecipe);
-			
-			smoothBirchLogRecipe.shape("   ", " LL", " LL");
-			p.getServer().addRecipe(smoothBirchLogRecipe);
-			
-			smoothBirchLogRecipe.shape("   ", "LL ", "LL ");
-			p.getServer().addRecipe(smoothBirchLogRecipe);
-		}
-	}
-	
-	public void registerJungle() {
-		if(p.getConfig().getBoolean("blocks.logs.jungle.craftable")) {
-			ShapedRecipe smoothJungleLogRecipe = new ShapedRecipe(p.getSmoothJungleItem(4));
-	
-			smoothJungleLogRecipe.shape("LL ", "LL ", "   ");
-			smoothJungleLogRecipe.setIngredient('L', Material.LOG, 3);
-			p.getServer().addRecipe(smoothJungleLogRecipe);
-			
-			smoothJungleLogRecipe.shape(" LL", " LL", "   ");
-			p.getServer().addRecipe(smoothJungleLogRecipe);
-			
-			smoothJungleLogRecipe.shape("   ", " LL", " LL");
-			p.getServer().addRecipe(smoothJungleLogRecipe);
-			
-			smoothJungleLogRecipe.shape("   ", "LL ", "LL ");
-			p.getServer().addRecipe(smoothJungleLogRecipe);
-		}
-	}
-	
-	public void registerAcacia() {
-		if(p.getConfig().getBoolean("blocks.logs.acacia.craftable")) {
-			ShapedRecipe smoothAcaciaLogRecipe = new ShapedRecipe(p.getSmoothAcaciaItem(4));
-	
-			smoothAcaciaLogRecipe.shape("LL ", "LL ", "   ");
-			smoothAcaciaLogRecipe.setIngredient('L', Material.LOG_2, 0);
-			p.getServer().addRecipe(smoothAcaciaLogRecipe);
-			
-			smoothAcaciaLogRecipe.shape(" LL", " LL", "   ");
-			p.getServer().addRecipe(smoothAcaciaLogRecipe);
-			
-			smoothAcaciaLogRecipe.shape("   ", " LL", " LL");
-			p.getServer().addRecipe(smoothAcaciaLogRecipe);
-			
-			smoothAcaciaLogRecipe.shape("   ", "LL ", "LL ");
-			p.getServer().addRecipe(smoothAcaciaLogRecipe);
-		}
-	}
-	
-	public void registerDarkOak() {
-		if(p.getConfig().getBoolean("blocks.logs.dark_oak.craftable")) {
-			ShapedRecipe smoothDarkOakLogRecipe = new ShapedRecipe(p.getSmoothDarkOakItem(4));
-	
-			smoothDarkOakLogRecipe.shape("LL ", "LL ", "   ");
-			smoothDarkOakLogRecipe.setIngredient('L', Material.LOG_2, 1);
-			p.getServer().addRecipe(smoothDarkOakLogRecipe);
-			
-			smoothDarkOakLogRecipe.shape(" LL", " LL", "   ");
-			p.getServer().addRecipe(smoothDarkOakLogRecipe);
-			
-			smoothDarkOakLogRecipe.shape("   ", " LL", " LL");
-			p.getServer().addRecipe(smoothDarkOakLogRecipe);
-			
-			smoothDarkOakLogRecipe.shape("   ", "LL ", "LL ");
-			p.getServer().addRecipe(smoothDarkOakLogRecipe);
-		}
-	}
-	
 }
