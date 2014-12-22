b8ceb8dc04e8d84469925b2d1f52a76d7cc8a2ee
Amaury Carrade <amaury@carrade.eu>
2014-12-22 13:01:04 +0100

    Code style

--- src/main/java/eu/carrade/amaury/BelovedBlocks/BBListener.java
+++ src/main/java/eu/carrade/amaury/BelovedBlocks/BBListener.java
@@ -187,6 +187,7 @@ public class BBListener implements Listener {
 	public void onBlockBreaks(BlockBreakEvent ev) {
 		Material itemInHandType = ev.getPlayer().getItemInHand().getType();
 		Block block = ev.getBlock();
+		
 		// This event only concerns players in survival game mode.
 		if(ev.getPlayer().getGameMode() != GameMode.CREATIVE) {
 			if(p.isValidTool(ev.getPlayer().getItemInHand())) {
@@ -198,15 +199,17 @@ public class BBListener implements Listener {
 						// The tool doesn't loose any durability though.
 					ev.setCancelled(true);
 					ev.getBlock().setType(Material.AIR);
-				}else if(ev.getBlock().getType() == Material.LEAVES
+				}
+				else if(ev.getBlock().getType() == Material.LEAVES
 						|| ev.getBlock().getType() == Material.LEAVES_2
 						|| ev.getBlock().getType() == Material.WOOL
 						|| ev.getBlock().getType() == Material.WEB
-						|| ev.getBlock().getType() == Material.STRING){
+						|| ev.getBlock().getType() == Material.STRING) {
 							
 						// The tool loses 2 durability points.
 					short newDurability = (short) (ev.getPlayer().getItemInHand().getDurability()
 							+ 2 * p.increaseDurability(ev.getPlayer().getItemInHand().getEnchantmentLevel(Enchantment.DURABILITY)));
+					
 					if(newDurability > ev.getPlayer().getItemInHand().getType().getMaxDurability()) {
 						ev.getPlayer().getInventory().setItemInHand(new ItemStack(Material.AIR));
 						ev.getPlayer().playSound(ev.getPlayer().getLocation(), Sound.ITEM_BREAK, 0.8f, 1);
@@ -214,64 +217,70 @@ public class BBListener implements Listener {
 					else {
 						ev.getPlayer().getItemInHand().setDurability(newDurability);
 					}
+					
 					ev.getPlayer().updateInventory();
 					ev.setCancelled(true);
 					ev.getBlock().setType(Material.AIR);
 				}
-				
-				// When you break a smooth double slab, our item is dropped.
-			}else if(itemInHandType == Material.WOOD_PICKAXE
+			}
+			
+			// When you break a smooth double slab, our item is dropped.
+			else if(itemInHandType == Material.WOOD_PICKAXE
 						|| itemInHandType == Material.STONE_PICKAXE
 						|| itemInHandType == Material.IRON_PICKAXE
 						|| itemInHandType == Material.GOLD_PICKAXE
 						|| itemInHandType == Material.DIAMOND_PICKAXE) {
 				
-					if(block.getType() == Material.DOUBLE_STEP) {
-						if(block.getData() == 8) {
-							ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothStoneItem(1));
-							ev.getBlock().setType(Material.AIR);
-						}
-						else if(block.getData() == 9) {
-							ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothSandstoneItem(1));
-							ev.getBlock().setType(Material.AIR);
-						}
-					}
-					else if(block.getType() == Material.DOUBLE_STONE_SLAB2 && block.getData() == 8) {
-						ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothRedSandstoneItem(1));
+				if(block.getType() == Material.DOUBLE_STEP) {
+					if(block.getData() == 8) {
+						ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothStoneItem(1));
 						ev.getBlock().setType(Material.AIR);
 					}
+					else if(block.getData() == 9) {
+						ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothSandstoneItem(1));
+						ev.getBlock().setType(Material.AIR);
+					}
+				}
+				else if(block.getType() == Material.DOUBLE_STONE_SLAB2 && block.getData() == 8) {
+					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothRedSandstoneItem(1));
+					ev.getBlock().setType(Material.AIR);
+				}
 
-				}
+			}
+			
 			if(block.getType() == Material.LOG) {
-				if(block.getData() == 12){
-				ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothOakItem(1));
-				ev.getBlock().setType(Material.AIR);
+				if(block.getData() == 12) {
+					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothOakItem(1));
+					ev.getBlock().setType(Material.AIR);
 				}
-				else if(block.getData() == 13){
+				else if(block.getData() == 13) {
 					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothSpruceItem(1));
 					ev.getBlock().setType(Material.AIR);
-					}
-				else if(block.getData() == 14){
+				}
+				else if(block.getData() == 14) {
 					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothBirchItem(1));
 					ev.getBlock().setType(Material.AIR);
-					}
-				else if(block.getData() == 15){
+				}
+				else if(block.getData() == 15) {
 					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothJungleItem(1));
 					ev.getBlock().setType(Material.AIR);
-					}
+				}
 			}
+			
 			else if(block.getType() == Material.LOG_2){
-				if(block.getData() == 12){
+				if(block.getData() == 12) {
 					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothAcaciaItem(1));
 					ev.getBlock().setType(Material.AIR);
-				}else if(block.getData() == 13){
+				}
+				else if(block.getData() == 13) {
 					ev.getPlayer().getWorld().dropItemNaturally(block.getLocation(), p.getSmoothDarkOakItem(1));
 					ev.getBlock().setType(Material.AIR);
 				}
-				
-			}
 			}
 		}
+	}
+	
+	
 	/**
 	 * Used to prevent our tool from shearing sheeps or mushroom cows.
 	 * <p>
@@ -310,51 +319,73 @@ public class BBListener implements Listener {
 		}
 	}
 	
-private ItemStack getItemStack(Inventory i) {
-	for(int t = 1 ; t < i.getSize() ; t++) {
-		if(i.getItem(t) != null && i.getItem(t).getType() != Material.AIR) {
-		return i.getItem(t);	
-		}
-	}
-	return null;
-}
-	
-private int getCount(Inventory i) {
-	int amount = 0;
-	for(int t = 1 ; t < i.getSize() ; t++) {
-		if(i.getItem(t) != null && i.getItem(t).getType() != Material.AIR) {
-	if(i.getItem(t).getAmount() != 0) {
-		amount ++;
+	/**
+	 * Returns the first non-empty stack inside the give inventory.
+	 * 
+	 * @param i The inventory
+	 * @return The ItemStack
+	 */
+	private ItemStack getItemStack(Inventory i) {
+		for(int t = 1 ; t < i.getSize() ; t++) {
+			if(i.getItem(t) != null && i.getItem(t).getType() != Material.AIR) {
+				return i.getItem(t);	
 			}
 		}
+		
+		return null;
 	}
-return amount;
-}
 	
+	/**
+	 * Returns the amount of non-empty stacks in the given inventory.
+	 * 
+	 * @param i The inventory.
+	 * @return The amount.
+	 */
+	private int getCount(Inventory i) {
+		int amount = 0;
+		for(int t = 1 ; t < i.getSize() ; t++) {
+			if(i.getItem(t) != null && i.getItem(t).getType() != Material.AIR) {
+				if(i.getItem(t).getAmount() != 0) {
+					amount++;
+				}
+			}
+		}
+		
+		return amount;
+	}
+	
+	/**
+	 * Used to filter reversed crafts.
+	 * 
+	 * @param e
+	 */
 	@EventHandler
 	public void onPreCraftEvent(PrepareItemCraftEvent e) {
 		ItemStack item = getItemStack(e.getInventory());
+		
 		if(getCount(e.getInventory()) == 1 && item != null && item.getType() != Material.AIR) {
+			// Handles the smooth stone items.
 			if(item.getType() == Material.STONE || item.getType() == Material.SANDSTONE || item.getType() == Material.RED_SANDSTONE) {
-				// Handles the smooth stone items.
 				if(item.hasItemMeta() && (item.equals(p.getSmoothStoneItem(item.getAmount())) || item.equals(p.getSmoothSandstoneItem(item.getAmount())) || item.equals(p.getSmoothRedSandstoneItem(item.getAmount())))) {
-					}else{
-						e.getInventory().setResult(new ItemStack(Material.AIR, 1));
-		}
-	}
-	else if(item.getType() == Material.LOG || item.getType() == Material.LOG_2){
-		// Handles the log items.
-			if(item.hasItemMeta() && (item.equals(p.getSmoothOakItem(item.getAmount()))
-					|| item.equals(p.getSmoothSpruceItem(item.getAmount()))
-					|| item.equals(p.getSmoothBirchItem(item.getAmount()))
-					|| item.equals(p.getSmoothJungleItem(item.getAmount()))
-					|| item.equals(p.getSmoothAcaciaItem(item.getAmount()))
-					|| item.equals(p.getSmoothDarkOakItem(item.getAmount())))) {
-				ItemStack result = new ItemStack(item.getType(), item.getDurability());
-				result.setAmount(1);
-				e.getInventory().setResult(result);
+				
+				}
+				else {
+					e.getInventory().setResult(new ItemStack(Material.AIR, 1));
+				}
+			}
 			
-			
+			// Handles the log items.
+			else if(item.getType() == Material.LOG || item.getType() == Material.LOG_2){
+				if(item.hasItemMeta() && (item.equals(p.getSmoothOakItem(item.getAmount()))
+						|| item.equals(p.getSmoothSpruceItem(item.getAmount()))
+						|| item.equals(p.getSmoothBirchItem(item.getAmount()))
+						|| item.equals(p.getSmoothJungleItem(item.getAmount()))
+						|| item.equals(p.getSmoothAcaciaItem(item.getAmount()))
+						|| item.equals(p.getSmoothDarkOakItem(item.getAmount())))) {
+					
+					ItemStack result = new ItemStack(item.getType(), item.getDurability());
+					result.setAmount(1);
+					e.getInventory().setResult(result);
 				}
 			}
 		}
